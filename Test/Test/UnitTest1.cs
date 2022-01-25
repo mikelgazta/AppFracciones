334ac7db91930c65e7a4008a2cb5f0028c8998af
1daw3 <1daw3@HZ321172.H012108.NET>
2022-01-25 08:58:35 +0100

    Test

--- Test/Test/UnitTest1.cs
+++ Test/Test/UnitTest1.cs
@@ -21,26 +21,40 @@ namespace Test
         public void multiplicacion()
         {
             // Given
-            var f1 =new Fraccion(1,2);
-            var f2 =new Fraccion (2,4);
+            var f1 = new Fraccion(1, 2);
+            var f2 = new Fraccion(2, 4);
             // When
-            var esperado="2/8";
-            var resultado=f1*f2;
+            var esperado = "2/8";
+            var resultado = f1 * f2;
             // Then
-            Assert.Equal(esperado,resultado.ToString());
+            Assert.Equal(esperado, resultado.ToString());
         }
         [Fact]
         public void resto()
         {
             // Given
-            var f1= new Fraccion(4,5);
-            var f2=new Fraccion(2,3);
+            var f1 = new Fraccion(4, 5);
+            var f2 = new Fraccion(2, 3);
             // When
-            var esperado="2/15";
-            var resultado=f1-f2;
+            var esperado = "2/15";
+            var resultado = f1 - f2;
             // Then
-            Assert.Equal(esperado,resultado.ToString());
+            Assert.Equal(esperado, resultado.ToString());
         }
+        [Fact]
+        public void resto_y_resultado_negativo()
+        {
+            // Given
+            var f1 = new Fraccion(3, 5);
+            var f2 = new Fraccion(5, 3);
+            // When
+            var esperado = "-16/15";
+            var resultado = f1 - f2;
+            // Then
+            Assert.Equal(esperado, resultado.ToString());
+        }
+
+
     }
 
 }
